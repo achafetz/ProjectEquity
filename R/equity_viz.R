74e5209f28c4eea16487c4fb80f3970001543d6d
Aaron <achafetz@usaid.gov>
2018-04-03 09:20:06 -0400

    add COP filter, start on#2

--- R/equity_viz.R
+++ R/equity_viz.R
@@ -12,9 +12,8 @@ library(skimr)
 library(knitr)
 library(scales)
 
-# M&O Categories ----------------------------------------------------------
 
-# 1. We need broad M&O categories for all agencies individually (not just CDC/USAID comparisons)
+# 1. broad M&O categories for all agencies individually -------------------
 
   #inspect
     staffing %>% skim(fundingagency_consol, Employment_Type, Position_Type)
@@ -32,17 +31,20 @@ library(scales)
     
   #table - by employment type 
     staffing %>% 
+      filter(Cycle == "2018 COP") %>% 
       count(fundingagency_consol, Employment_Type) %>% 
       spread(Employment_Type, n) %>% 
       kable(format.args = list(big.mark = ",", zero.print = FALSE))
   #table - by position type
     staffing %>% 
+      filter(Cycle == "2018 COP") %>% 
       count(fundingagency_consol, Position_Type) %>% 
       spread(fundingagency_consol, n) %>% 
       kable(format.args = list(big.mark = ",", zero.print = FALSE))
     
   #viz - overall  
-    staffing %>% 
+    staffing %>%
+      filter(Cycle == "2018 COP") %>% 
       count(fundingagency_consol, Employment_Type) %>% 
       ggplot(aes(reorder(Employment_Type, n), n)) +
         geom_col() +
@@ -53,32 +55,60 @@ library(scales)
    #viz - by employment type
     staffing %>% 
       count(fundingagency_consol, Employment_Type) %>%
-      mutate(fundingagency_ordered = factor(fundingagency_consol, levels = c("USAID", "HHS/CDC", "PC", "State", "DOD", "HHS/Other")),
-             usaid = ifelse(fundingagency_consol == "USAID", 1, 0)) %>% 
+      filter(Cycle == "2018 COP") %>% 
+      mutate(usaid = ifelse(fundingagency_consol == "USAID", 1, 0)) %>% 
       ggplot(aes(reorder(Employment_Type, n), n)) +
         geom_col(aes(fill = usaid), show.legend = FALSE) +
         scale_fill_continuous(low = "#99c2eb", high = "#2166ac") +
         coord_flip() +
         labs(x = "", y = "# of staff") +
         scale_y_continuous(labels = comma) +
-        facet_grid(. ~ fundingagency_ordered)
+        facet_grid(. ~ fundingagency_consol)
      ggsave(here("Products", "prj_equity_q1_empl_type.png"), width = 11, height = 2.5, units = "in")
     
     #viz - by position type
      staffing %>% 
        count(fundingagency_consol, Position_Type) %>%
-       mutate(fundingagency_ordered = factor(fundingagency_consol, levels = c("USAID", "HHS/CDC", "PC", "State", "DOD", "HHS/Other")),
-              usaid = ifelse(fundingagency_consol == "USAID", 1, 0)) %>% 
+       filter(Cycle == "2018 COP") %>% 
+       mutate(usaid = ifelse(fundingagency_consol == "USAID", 1, 0)) %>% 
        ggplot(aes(reorder(Position_Type, n), n)) +
-       geom_col(aes(fill = usaid), show.legend = FALSE) +
-       scale_fill_continuous(low = "#99c2eb", high = "#2166ac") +
-       coord_flip() +
-       labs(x = "", y = "# of staff") +
-       scale_y_continuous(labels = comma) +
-       facet_grid(. ~ fundingagency_ordered)
+         geom_col(aes(fill = usaid), show.legend = FALSE) +
+         scale_fill_continuous(low = "#99c2eb", high = "#2166ac") +
+         coord_flip() +
+         labs(x = "", y = "# of staff") +
+         scale_y_continuous(labels = comma) +
+         facet_grid(. ~ fundingagency_consol)
      ggsave(here("Products", "prj_equity_q1_position_type.png"), width = 11, height = 2.5, units = "in")
   
-# 2. IM: We need better understanding of # of IMs managed by different agencies, by OU  
 
-  
+# 2. # of IMs managed by different agencies, by OU  -----------------------
 
+  # inspect
+    skim(copmatrix, COP, OperatingUnit,MechanismIdentifier)
+    distinct(copmatrix, COP)
+    distinct(copmatrix, MechanismIdentifier) %>% arrange(MechanismIdentifier)
+    
+  #table - global
+     copmatrix %>% 
+       filter(COP == "2018 COP", MechanismIdentifier != "0") %>% 
+       count(fundingagency_consol) %>% 
+       arrange(desc(n))
+     
+  #table - by OU
+     copmatrix %>% 
+       filter(COP == "2018 COP", MechanismIdentifier != "0") %>% 
+       count(OperatingUnit, fundingagency_consol) %>% 
+       spread(fundingagency_consol, n) %>% 
+       print(n = Inf)
+     
+  # viz - by OU
+     copmatrix %>% 
+       filter(COP == "2018 COP", MechanismIdentifier != "0") %>% 
+       count(OperatingUnit, fundingagency_consol) %>%
+       mutate(usaid = ifelse(fundingagency_consol == "USAID", 1, 0)) %>%
+       ggplot(aes(OperatingUnit, n)) +
+         geom_col(aes(fill = usaid), show.legend = FALSE) +
+         scale_fill_continuous(low = "#99c2eb", high = "#2166ac") +
+         coord_flip() +
+         facet_grid(. ~ fundingagency_consol) +
+         labs(x = "", y = "# of mechanisms", caption = "COP18")
