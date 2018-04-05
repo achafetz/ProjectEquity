b2ab6a74161e7384b70e378a026c7fc34a5efc4f
achafetz <achafetz@usaid.gov>
2018-04-05 16:58:57 -0400

    add second component

--- R/equity_viz-q2b.R
+++ R/equity_viz-q2b.R
@@ -16,15 +16,17 @@ library(scales)
 
 # Open Datasets -----------------------------------------------------------
 
-copmatrix <- read_rds("Output/copmatrix.Rds")
-codb_ptype <- read_rds("Output/codb_ptype.Rds")
+  codb_ptype <- read_rds("Output/codb_ptype.Rds")
 
-# 2a. # of IMs managed by different agencies, by OU  -----------------------
+# 2b. # of IMs managed by different agencies, by OU  -----------------------
 
-# inspect
-skim(copmatrix, COP, OperatingUnit,MechanismIdentifier)
-distinct(copmatrix, COP)
-distinct(copmatrix, MechanismIdentifier) %>% arrange(MechanismIdentifier)
-
-skim(codb_ptype, ProcurementType)
-distinct(codb_ptype, ProcurementType)
+  # inspect
+    skim(codb_ptype, ProcurementType)
+    distinct(codb_ptype, ProcurementType)
+  
+  #table - global
+    codb_ptype %>% 
+      filter(COP == "2018 COP", MechanismIdentifier != "0") %>% 
+      distinct(fundingagency_consol, ProcurementType, MechanismIdentifier) %>% 
+      count(OperatingUnit, fundingagency_consol) %>% 
+      
